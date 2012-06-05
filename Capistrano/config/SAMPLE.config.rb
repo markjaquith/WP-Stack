337aff40bcaca28d9c0eab995bcdb138caf551e7
Mark Jaquith <m@rkj.me>
2012-06-05 13:52:28 -0400

    Add in multistage

--- Capistrano/config/SAMPLE.config.rb
+++ Capistrano/config/SAMPLE.config.rb
@@ -2,22 +2,10 @@
 
 set :application, "WP Stack Site"
 set :repository,  "set your git repository location here"
-set :deploy_to, "/srv/www/example.com"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 # Using Git Submodules?
 set :git_enable_submodules, 1
 
-# role :web, "your web server here"
-# role :web, "second web server here"
-# role :web, "third web server here, etc"
-
-# role :memcached, "your memcached server here"
-# role :memcached, "second memcached server here, etc"
-
-# Alternatively, if your web servers are the same as your memcached servers,
-# comment out all the "role" lines and use "server" lines:
-
-# server "your web/memcached server here", :web, :memcached
-# server "second web/memcached server here", :web, :memcached
+# You're not done! You must also configure production.rb and staging.rb
---
+++ Capistrano/config/SAMPLE.staging.rb
@@ -0,0 +1,24 @@
+# This file is only loaded for the staging environment
+# Customize it and rename it as staging.rb
+
+# Where should the site deploy to?
+set :deploy_to, "/srv/www/example.com"
+
+# Now configure the servers for this environment
+
+# OPTION 1
+
+# role :web, "your web server here"
+# role :web, "second web server here"
+# role :web, "third web server here, etc"
+
+# role :memcached, "your memcached server here"
+# role :memcached, "second memcached server here, etc"
+
+# OPTION 2
+
+# If your web servers are the same as your memcached servers,
+# comment out all the "role" lines and use "server" lines:
+
+# server "your web/memcached server here", :web, :memcached
+# server "second web/memcached server here", :web, :memcached
