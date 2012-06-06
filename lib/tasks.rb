6c49d1db1dca6a797ec21420a40374df84fa7ab6
Mark Jaquith <m@rkj.me>
2012-06-06 00:54:14 -0400

    Set WP_Stage in wp-config.php, so people can do logic based on stage

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -66,9 +66,9 @@ namespace :db do
       end
     end
 	end
-	desc "Sets the database credentials in wp-config.php"
+	desc "Sets the database credentials (and other settings) in wp-config.php"
 	task :make_config do
-		{:'%%DB_NAME%%' => wpdb[stage][:name], :'%%DB_USER%%' => wpdb[stage][:user], :'%%DB_PASSWORD%%' => wpdb[stage][:password], :'%%DB_HOST%%' => wpdb[stage][:host]}.each do |k,v|
+		{:'%%WP_STAGE%%' => stage, :'%%DB_NAME%%' => wpdb[stage][:name], :'%%DB_USER%%' => wpdb[stage][:user], :'%%DB_PASSWORD%%' => wpdb[stage][:password], :'%%DB_HOST%%' => wpdb[stage][:host]}.each do |k,v|
 			run "sed -i 's/#{k}/#{v}/' #{release_path}/wp-config.php", :roles => :web
 		end
 	end
