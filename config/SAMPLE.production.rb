03d4e233964a1eff9147da1477a113f20d1985b6
Mark Jaquith <m@rkj.me>
2012-06-07 16:51:54 -0400

    Make it clear that we want an IP address or a hostname

--- config/SAMPLE.production.rb
+++ config/SAMPLE.production.rb
@@ -8,11 +8,11 @@ set :deploy_to, "/srv/www/example.com"
 
 # OPTION 1
 
-# role :web, "your web server here"
+# role :web, "your web server IP address or hostname here"
 # role :web, "second web server here"
 # role :web, "third web server here, etc"
 
-# role :memcached, "your memcached server here"
+# role :memcached, "your memcached server IP address or hostname here"
 # role :memcached, "second memcached server here, etc"
 
 # OPTION 2
@@ -20,5 +20,5 @@ set :deploy_to, "/srv/www/example.com"
 # If your web servers are the same as your memcached servers,
 # comment out all the "role" lines and use "server" lines:
 
-# server "your web/memcached server here", :web, :memcached
+# server "your web/memcached server IP address or hostname here", :web, :memcached
 # server "second web/memcached server here", :web, :memcached
