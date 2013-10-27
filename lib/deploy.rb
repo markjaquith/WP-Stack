5da9dc4073d5622fb6f38380cc35c1cf92013630
Mark Jaquith <mark@jaquith.me>
2013-10-27 13:49:04 -0400

    Do cleanup after deploy:update

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -5,7 +5,7 @@ set :deploy_via, :remote_cache
 set :copy_exclude, [".git", ".gitmodules", ".DS_Store", ".gitignore"]
 set :keep_releases, 5
 
-after "deploy:restart", "deploy:cleanup"
+after "deploy:update", "deploy:cleanup"
 after "deploy:update_code", "shared:make_shared_dir"
 after "deploy:update_code", "shared:make_symlinks"
 after "deploy:update_code", "db:make_config"
