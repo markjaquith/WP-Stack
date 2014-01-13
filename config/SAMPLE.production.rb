44e36c5c6179bf7f41bf750f9539175fb80eb2c8
andrezrv <andrezrv@gmail.com>
2014-01-13 03:32:34 +0000

    :deploy_to and first :web values are obtained from config.rb

--- config/SAMPLE.production.rb
+++ config/SAMPLE.production.rb
@@ -1,14 +1,17 @@
-# This file is only loaded for the production environment
-# Customize it and rename it as production.rb
+# production.rb
+#
+# This file is only loaded for the production stage.
+# Customize it and copy it as production.rb.
 
 # Where should the site deploy to?
-set :deploy_to, "/srv/www/example.com"
+set :deploy_to, :production_deploy_to
 
 # Now configure the servers for this environment
 
 # OPTION 1
+# Your web servers IP addresses or hostnamen go here
 
-# role :web, "your web server IP address or hostname here"
+role :web, :production_domain
 # role :web, "second web server here"
 # role :web, "third web server here, etc"
 
