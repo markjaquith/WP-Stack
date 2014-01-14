6c36dff5b27f126d614b5c882130bf4fe4fd107d
andrezrv <andrezrv@gmail.com>
2014-01-14 14:26:15 +0000

    Added inline docs.

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -2,18 +2,21 @@
 # 
 # This file includes default settings and hooks for the deployment process.
 
+# Set some default values
 set :user, "deploy"
 set :use_sudo, false
 set :deploy_via, :remote_cache
 set :copy_exclude, [".git", ".gitmodules", ".DS_Store", ".gitignore"]
 set :keep_releases, 5
 
+# Put some hooks before deploy tasks
 after "deploy:update", "deploy:cleanup"
 after "deploy:update_code", "shared:make_shared_dir"
 after "deploy:update_code", "shared:make_symlinks"
 after "deploy:update_code", "db:make_config"
 after "deploy", "memcached:update"
 
+# Put some hooks after deploy tasks
 before "deploy:setup", "db:init"
 
 # Pull in the config file
